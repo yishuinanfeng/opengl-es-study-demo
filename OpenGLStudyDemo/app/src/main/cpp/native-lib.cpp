906c21279298dc037b469022b2cabcf633bd9da1
yishuinanfeng <yanxiaxue@qq.com>
2022-10-23 23:14:20 +0800

    渲染yuv视频着色器GLSL升级为3.0

--- OpenGLStudyDemo/app/src/main/cpp/native-lib.cpp
+++ OpenGLStudyDemo/app/src/main/cpp/native-lib.cpp
@@ -82,20 +82,23 @@ static const char *vertexSimpleShapeWithColor =
 
 //顶点着色器，每个顶点执行一次，可以并行执行
 #define GET_STR(x) #x
-static const char *vertexShader = GET_STR(
-        attribute
-        vec4 aPosition;//输入的顶点坐标，会在程序指定将数据输入到该字段
-        attribute
-        vec2 aTextCoord;//输入的纹理坐标，会在程序指定将数据输入到该字段
-        varying
-        vec2 vTextCoord;//输出的纹理坐标
-        void main() {
-            //这里其实是将上下翻转过来（因为安卓图片会自动上下翻转，所以转回来）
-            vTextCoord = vec2(aTextCoord.x, 1.0 - aTextCoord.y);
-            //直接把传入的坐标值作为传入渲染管线。gl_Position是OpenGL内置的
-            gl_Position = aPosition;
-        }
-);
+static const char *vertexShader =
+        "        #version 300 es\n"
+        "        layout (location = 0) \n"
+        "        in vec4 aPosition;//输入的顶点坐标，会在程序指定将数据输入到该字段\n"//如果传入的向量是不够4维的，自动将前三个分量设置为0.0，最后一个分量设置为1.0
+
+        "        layout (location = 1) \n"
+        "        in vec2 aTextCoord;//输入的纹理坐标，会在程序指定将数据输入到该字段\n"
+        "\n"
+        "        out\n"
+        "        vec2 vTextCoord;//输出的纹理坐标;\n"
+        "\n"
+        "        void main() {\n"
+        "            //这里其实是将上下翻转过来（因为安卓图片会自动上下翻转，所以转回来）\n"
+        "             vTextCoord = vec2(aTextCoord.x, 1.0 - aTextCoord.y);\n"
+        "            //直接把传入的坐标值作为传入渲染管线。gl_Position是OpenGL内置的\n"
+        "            gl_Position = aPosition;\n"
+        "        }";
 
 //图元被光栅化为多少片段，就被调用多少次
 static const char *fragSimpleShape =
@@ -178,35 +181,32 @@ static const char *vertexSimpleUniform =
         "        }";
 
 //图元被光栅化为多少片段，就被调用多少次
-static const char *fragYUV420P = GET_STR(
-        precision
-        mediump float;
-        varying
-        vec2 vTextCoord;
+static const char *fragYUV420P =
+        "#version 300 es\n"
+
+        "precision mediump float;\n"
+
+        "in vec2 vTextCoord;\n"
         //输入的yuv三个纹理
-        uniform
-        sampler2D yTexture;//采样器
-        uniform
-        sampler2D uTexture;//采样器
-        uniform
-        sampler2D vTexture;//采样器
-        void main() {
-            vec3 yuv;
-            vec3 rgb;
-            //分别取yuv各个分量的采样纹理（r表示？）
-            //
-            yuv.x = texture2D(yTexture, vTextCoord).r;
-            yuv.y = texture2D(uTexture, vTextCoord).g - 0.5;
-            yuv.z = texture2D(vTexture, vTextCoord).b - 0.5;
-            rgb = mat3(
-                    1.0, 1.0, 1.0,
-                    0.0, -0.39465, 2.03211,
-                    1.13983, -0.5806, 0.0
-            ) * yuv;
-            //gl_FragColor是OpenGL内置的
-            gl_FragColor = vec4(rgb, 1.0);
-        }
-);
+        "uniform sampler2D yTexture;//采样器\n"
+        "uniform sampler2D uTexture;//采样器\n"
+        "uniform sampler2D vTexture;//采样器\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "   vec3 yuv;\n"
+        "    vec3 rgb;\n"
+        "    //分别取yuv各个分量的采样纹理（r表示？）\n"
+        "    yuv.x = texture(yTexture, vTextCoord).r;\n"
+        "   yuv.y = texture(uTexture, vTextCoord).g - 0.5;\n"
+        "    yuv.z = texture(vTexture, vTextCoord).b - 0.5;\n"
+        "   rgb = mat3(\n"
+        "            1.0, 1.0, 1.0,\n"
+        "            0.0, -0.39465, 2.03211,\n"
+        "            1.13983, -0.5806, 0.0\n"
+        "    ) * yuv;\n"
+        "    //gl_FragColor是OpenGL内置的\n"
+        "    FragColor = vec4(rgb, 1.0);\n"
+        " }";
 
 
 extern "C" JNIEXPORT jstring JNICALL
